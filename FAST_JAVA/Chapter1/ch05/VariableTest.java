ddd81c495c2915b61912cc2996ef933f80aec9a0
HeeSeok Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-09-02 20:47:23 +0900

    Add files via upload

---
+++ FAST_JAVA/Chapter1/ch05/VariableTest.java
@@ -0,0 +1,13 @@
+package ch05;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int age, count;
+		age = 10;
+		count = 100;
+		int level = 10000;
+		System.out.printf("%d %d\n",age, count);
+		System.out.println(level);
+	}
+}
