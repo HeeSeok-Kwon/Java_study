f4a64cdcf0538c83f254017890e2421d714414e6
HeeSeok Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-12-21 21:55:13 +0900

    Delete FAST_JAVA directory

--- FAST_JAVA/Chapter1/ch01/HelloJava.java
+++
@@ -1,8 +0,0 @@
-package ch01;
-
-public class HelloJava {
-
-	public static void main(String[] args) { // main : 구동하기 위한 파일
-		System.out.println("Hello, Java");
-	}
-}
--- FAST_JAVA/README.md
+++
@@ -1,4 +0,0 @@
-## JAVA
-- package(소문자) - class(대문자로 시작)
-- .java(코딩파일) - .class(실행파일)
-- HelloJava.java인 경우 public class는 반드시 HelloJava 하나
